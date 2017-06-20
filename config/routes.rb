5326f7059be5ba53a43b399bd0187fdbadb81901
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-06-20 17:46:56 +0000

    integration of friendly routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
-  get 'About', to: 'pages#About'
-  get 'Contact', to: 'pages#Contact'
+  get 'about', to: 'pages#About'
+  get 'contact', to: 'pages#Contact'
 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
