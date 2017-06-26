24f1ea3d7f621948cda1641e57aa42f67381520a
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-06-26 17:06:45 +0000

    Fixed bugs and more

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,12 +8,12 @@ class Portfolio < ApplicationRecord
         where(subtitle: 'Angular')
     end
     
-    scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
+    scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
     
     after_initialize :set_defaults
     
     def set_defaults
-       self.main_image ||= "Placeholder.image_generator(height: '600', width: '400')"
-       self.thumb_image ||= "Placeholder.image_generator(height: '350', width: '200')"
+       self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+       self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
     end
 end
