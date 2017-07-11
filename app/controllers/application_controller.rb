fa34d1d98a35d2fba78d00d595992033b0cbe6a6
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-07-11 17:21:21 +0000

    Added my own rubygem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,19 @@ class ApplicationController < ActionController::Base
   include SetSource 
   include CurrentUserConcern
   include DefaultPageContent
-
+  
+  before_action :set_copyright
+  
+  def set_copyright
+    @copyright = Jeffries::Renderer.copyright 'Samuel Jeffries', 'All rights reserved.'
+  end
 end
+
+module Jeffries
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
+end
+      
